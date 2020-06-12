2b51796a3cc66d2b8c1b4cf74756254f596d3ee8
Tushar Joshi <tushar.jo@endurance.com>
2020-06-12 21:50:47 +0530

    Added express-session

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 const express = require("express");
 const path = require("path");
 const bodyParser = require("body-parser");
+var session = require("express-session");
 const cors = require("cors");
-const app = express();
+var app = express();
 
-let localSessionState = {};
+var sess = {
+  secret: "keyboard cat",
+  cookie: {},
+};
+
+if (app.get("env") === "production") {
+  app.set("trust proxy", 1); // trust first proxy
+  sess.cookie.secure = true; // serve secure cookies
+}
+
+app.use(session(sess));
 let clearedMessage = "";
 
 app.use(cors());
@@ -13,14 +24,21 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 app.set("view engine", "pug");
 
+app.use(function (req, res, next) {
+  if (!req.session.data) {
+    req.session.data = {};
+  }
+  next();
+});
+
 app.get("/", function (req, res) {
-  res.render("index", { session: localSessionState, clearedMessage });
+  res.render("index", { session: req.session.data, clearedMessage });
   clearedMessage = "";
 });
 
 // Clear all apis
 app.get("/clear", function (req, res) {
-  localSessionState = {};
+  req.session.data = {};
   clearedMessage = "All API(s) Cleared! Thanks!";
   res.redirect("/");
 });
@@ -28,17 +46,17 @@ app.get("/clear", function (req, res) {
 // Submit handler
 app.post("/submit", function (req, res) {
   const callName = Math.floor(Math.random() * 200) + 1;
-  localSessionState[callName] = req.body.jsondata;
+  req.session.data[callName] = req.body.jsondata;
   res.redirect("/");
 });
 
 // Call router
 app.get("/app/:name", function (req, res) {
   res.setHeader("Content-Type", "application/json");
-  if (localSessionState[req.params.name]) {
-    res.send(localSessionState[req.params.name]);
+  if (req.session.data[req.params.name]) {
+    res.send(req.session.data[req.params.name]);
   } else {
-    res.send({ error: "No API call found" });
+    res.send({ error: "No API call found!" });
   }
 });
 
