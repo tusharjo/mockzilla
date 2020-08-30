7935dd09a4777b83de7b4916f5e2daf4041b22ad
Tushar Joshi <tushar.jo@endurance.com>
2020-08-30 19:35:49 +0530

    Added location protocol

--- client/src/config.tsx
+++ client/src/config.tsx
@@ -1,6 +1,6 @@
 const endpoints = {
   APP_URL: process.env.REACT_APP_API_URI ? process.env.REACT_APP_API_URI : "",
-  COPY_URL: process.env.REACT_APP_API_URI ? process.env.REACT_APP_API_URI : window.location.hostname
+  COPY_URL: process.env.REACT_APP_API_URI ? process.env.REACT_APP_API_URI : `${window.location.protocol}${window.location.hostname}`
 };
 
 export default {
